8db39e9e4032afb0738829ebd90aab64f7ee9ff5
YuliaVyaznikova <twthlmn@gmail.com>
2024-09-07 14:22:49 +0700

    sample

---
+++ Task_1_1_1/src/test/java/ru/nsu/vyaznikova/MainTest.java
@@ -0,0 +1,10 @@
+package ru.nsu.vyaznikova;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void SampleTest() {
+        assertEquals(3, Main.sum(1, 2));
+    }
+}
