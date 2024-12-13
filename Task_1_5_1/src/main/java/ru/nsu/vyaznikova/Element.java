e7958298074675a6b652584f43258ab8adc2ebb1
YuliaVyaznikova <y.vyaznikova@g.nsu.ru>
2024-12-13 13:16:38 +0700

    code style changed againnn

--- Task_1_5_1/src/main/java/ru/nsu/vyaznikova/Element.java
+++ Task_1_5_1/src/main/java/ru/nsu/vyaznikova/Element.java
@@ -1,35 +1,44 @@
 package ru.nsu.vyaznikova;
 
 /**
- * Abstract base class for all Markdown elements.
- * This class provides the foundation for creating various Markdown elements
- * such as text formatting, tables, lists, headers, etc.
+ * Abstract class representing a Markdown element.
+ * This is the base class for all Markdown elements in the library.
  */
 public abstract class Element {
     
     /**
      * Converts the element to its Markdown representation.
-     * @return A string containing the Markdown representation of the element
+     * Each subclass must implement this method to provide its specific Markdown format.
+     *
+     * 
+     * @return the Markdown string representation of the element
      */
     public abstract String toMarkdown();
 
     /**
-     * Compares this element to the specified object for equality.
-     * Two elements are considered equal if they are of the same class
-     * and their Markdown representations are identical.
-     * @param obj the object to compare this element against
-     * @return true if the given object is equal to this element, false otherwise
+     * Checks if this element is equal to another object.
+     * Two elements are considered equal if their Markdown representations are identical.
+     *
+     * 
+     * @param obj the object to compare with
+     * @return true if the objects are equal, false otherwise
      */
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Element)) {
+            return false;
+        }
         return toMarkdown().equals(((Element) obj).toMarkdown());
     }
 
     /**
      * Returns a hash code value for the element.
      * The hash code is based on the element's Markdown representation.
+     *
+     * 
      * @return a hash code value for this element
      */
     @Override
