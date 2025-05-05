abe0a5b5009bbfafec3dc5125d7b6e3f7e1b7d9e
YuliaVyaznikova <y.vyaznikova@g.nsu.ru>
2025-05-06 02:34:52 +0700

    message class added

---
+++ Task_2_1_2/src/main/java/ru/nsu/vyaznikova/Message.java
@@ -0,0 +1,62 @@
+package ru.nsu.vyaznikova;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Message implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    public enum MessageType {
+        TASK,
+        RESULT,
+        HEARTBEAT,
+        ERROR
+    }
+    
+    private final MessageType type;
+    private final String content;
+    private final long timestamp;
+    
+    public Message(MessageType type, String content) {
+        this.type = Objects.requireNonNull(type, "Message type cannot be null");
+        this.content = Objects.requireNonNull(content, "Message content cannot be null");
+        this.timestamp = System.currentTimeMillis();
+    }
+    
+    public MessageType getType() {
+        return type;
+    }
+    
+    public String getContent() {
+        return content;
+    }
+    
+    public long getTimestamp() {
+        return timestamp;
+    }
+    
+    public boolean isExpired(long timeoutMs) {
+        return System.currentTimeMillis() - timestamp > timeoutMs;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return timestamp == message.timestamp &&
+               type == message.type &&
+               Objects.equals(content, message.content);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, content, timestamp);
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Message{type=%s, content=%s, timestamp=%d}", 
+            type, content, timestamp);
+    }
+}
