80954d8ba295342748781b6c2b43fcb5bf014427
YuliaVyaznikova <y.vyaznikova@g.nsu.ru>
2025-05-13 15:09:50 +0700

    codestyle in progressssss

--- Task_2_1_2/src/test/java/ru/nsu/vyaznikova/TaskPoolTest.java
+++ Task_2_1_2/src/test/java/ru/nsu/vyaznikova/TaskPoolTest.java
@@ -1,14 +1,12 @@
 package ru.nsu.vyaznikova;
 
-import java.util.Optional;
-import java.util.UUID;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Optional;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class TaskPoolTest {
     private TaskPool taskPool;
