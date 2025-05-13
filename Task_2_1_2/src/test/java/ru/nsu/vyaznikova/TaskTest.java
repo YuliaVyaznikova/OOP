70472d041923414f12c6b57de6dde2e4a144cec5
YuliaVyaznikova <y.vyaznikova@g.nsu.ru>
2025-05-13 15:03:50 +0700

    codestyle in progressss

--- Task_2_1_2/src/test/java/ru/nsu/vyaznikova/TaskTest.java
+++ Task_2_1_2/src/test/java/ru/nsu/vyaznikova/TaskTest.java
@@ -1,12 +1,12 @@
 package ru.nsu.vyaznikova;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 class TaskTest {
     @Test
