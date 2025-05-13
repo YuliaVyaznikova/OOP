77d4765972592c9181db7d8f7bbf09b5aebd8b7d
YuliaVyaznikova <y.vyaznikova@g.nsu.ru>
2025-05-13 15:43:35 +0700

    codestyle in progressssssssssssssssssssss

--- Task_2_1_2/src/main/java/ru/nsu/vyaznikova/PrimeChecker.java
+++ Task_2_1_2/src/main/java/ru/nsu/vyaznikova/PrimeChecker.java
@@ -1,7 +1,19 @@
 package ru.nsu.vyaznikova;
 
+/**
+ * Utility class for checking prime numbers.
+ * Provides methods to check if a single number is prime
+ * and to analyze arrays of numbers for non-prime values.
+ */
 public class PrimeChecker {
 
+    /**
+     * Checks if a given number is prime.
+     * A prime number is a natural number greater than 1 that is only divisible by 1 and itself.
+     *
+     * @param number the number to check for primality
+     * @return true if the number is prime, false otherwise
+     */
     public static boolean isPrime(int number) {
         if (number <= 1) {
             return false;
@@ -20,6 +32,12 @@ public class PrimeChecker {
         return true;
     }
 
+    /**
+     * Checks if an array contains any non-prime numbers.
+     *
+     * @param numbers array of integers to check
+     * @return true if any number in the array is not prime, false if all numbers are prime
+     */
     public static boolean hasNonPrime(int[] numbers) {
         for (int number : numbers) {
             if (!isPrime(number)) {
@@ -29,6 +47,12 @@ public class PrimeChecker {
         return false;
     }
 
+    /**
+     * Checks an array for non-prime numbers and returns a descriptive message.
+     *
+     * @param numbers array of integers to check
+     * @return a message indicating whether all numbers are prime or which number is non-prime
+     */
     public static String checkArrayResult(int[] numbers) {
         for (int number : numbers) {
             if (!isPrime(number)) {
